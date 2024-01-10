6222918bf3abae9f1fd65607ef962b13494aa73e
md-anamul-haque <m.anamul.dev@gmail.com>
2024-01-11 03:54:01 +0600

    solve to handle medialware

--- src/routers.ts
+++ src/routers.ts
@@ -1,10 +1,11 @@
 
-import { NextFunction, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { processConsole } from './lib/processConsole';
 import { readFiles } from "./lib/readFiles";
 import { createRoutePath, filterAndLowercaseHttpMethods } from "./lib/utils";
 import { writeToFileSyncStartupCode } from './lib/writeToFileSyncStartupCode';
 export type routesProps = (string | { baseDir: string; });
+
 export const routes = (config: routesProps) => {
     // const fileExtension = new Error().stack?.split("\n")[2].match(/\/([^\/]+)$/)?.[1]?.split('.').pop()?.split(':')?.[0];
 
@@ -31,20 +32,15 @@ export const routes = (config: routesProps) => {
             const exportFunctions = require(filename);
             const filteredHttpMethods = filterAndLowercaseHttpMethods(Object.keys(exportFunctions));// 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head' | 'connect' | 'trace' | 'copy' | 'lock' | 'move' | 'unlock' | 'propfind' | 'proppatch' | 'mkcol' | 'checkout' | 'search'
             filteredHttpMethods.forEach(method => {
-                // if (nameOfParamsMatch) {
-                router[method](apiUrl, (req: any, res: Response, next: NextFunction) => {
-                    if (nameOfParamsMatch && typeof nameOfParamsMatch == 'string') {
-                        const newProps = { ...req?.params, [nameOfParamsMatch as string]: req?.params?.[0]?.split('/') };
-                        let newRequest = { ...req, params: newProps };
-                        type NewReqType = typeof newRequest;
-                        return exportFunctions[method.toUpperCase()](newRequest as NewReqType, res, next)
-                    } else {
-                        return exportFunctions[method.toUpperCase()](req as Request, res, next)
-                    }
-                });
-                // } else {
-                //     router[method](apiUrl, exportFunctions[method.toUpperCase()])
-                // }
+                if (nameOfParamsMatch && typeof nameOfParamsMatch == 'string') {
+                    router[method](apiUrl, (req: Request, _res: Response, next: NextFunction) => {
+                        // @ts-ignore
+                        req.params[nameOfParamsMatch] = req?.params?.[0]?.split('/');
+                        next()
+                    }, exportFunctions[method.toUpperCase()])
+                } else {
+                    router[method](apiUrl, exportFunctions[method.toUpperCase()])
+                }
             });
         });
     }
